c1e637afa8e105b6051ad138c2474e822ab043ae
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2021-08-03 07:46:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 
-function preload(){
+var sea,seaImg1
 
-  seaImg = loadImage(sea.png);
+
+function preload() {
+
+  seaImg = loadImage("sea.png");
+
   shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
   }
 
@@ -9,14 +13,14 @@ function preload(){
 function setup(){
   createCanvas(400,400);
  
-
+//correction done
   sea = createSprite(400,200);
   sea.addImage(seaImg);
   sea.velocityX = -5;
   sea.scale = 0.25;
-
+//correction done
   ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip");
+  ship.addAnimation("shipImg1",shipImg1);
   ship.scale = 0.25;
 
 }
@@ -25,8 +29,9 @@ function draw() {
   background("blue");
   sea.velocityX = -3;
   
-  if(sea.X < 0){
-    sea.X = sea.width/2
+  if (sea.x < 0) {
+    //correction done
+    sea.x = width/2
   }
   
   drawSprites();

fd4645bab1f60088e389de012d205063fbee0f1a
RAJIVRAWAL <88193056+RAJIVRAWAL@users.noreply.github.com>
2021-08-04 22:52:32 +0530

    Revert "Update sketch.js"

--- sketch.js
+++ sketch.js
@@ -1,11 +1,7 @@
 
-var sea,seaImg1
+function preload(){
 
-
-function preload() {
-
-  seaImg = loadImage("sea.png");
-
+  seaImg = loadImage(sea.png);
   shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
   }
 
@@ -13,14 +9,14 @@ function preload() {
 function setup(){
   createCanvas(400,400);
  
-//correction done
+
   sea = createSprite(400,200);
   sea.addImage(seaImg);
   sea.velocityX = -5;
   sea.scale = 0.25;
-//correction done
+
   ship = createSprite(130,200,30,30);
-  ship.addAnimation("shipImg1",shipImg1);
+  ship.addAnimation("movingShip");
   ship.scale = 0.25;
 
 }
@@ -29,9 +25,8 @@ function draw() {
   background("blue");
   sea.velocityX = -3;
   
-  if (sea.x < 0) {
-    //correction done
-    sea.x = width/2
+  if(sea.X < 0){
+    sea.X = sea.width/2
   }
   
   drawSprites();
